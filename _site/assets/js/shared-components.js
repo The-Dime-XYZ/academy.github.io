f8723765f0177217dda53966a5eda34d325491e7
TheDimeXYZ <barry@thedime.xyz>
2025-06-02 21:28:07 +1000

    more content

--- _site/assets/js/shared-components.js
+++ _site/assets/js/shared-components.js
@@ -2,22 +2,44 @@
 
 function NavbarComponent() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm py-3">
       <div className="container">
-        <a className="navbar-brand" href="/">The Dime Academy</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <img src="/academy.github.io/assets/images/logo_black.png" alt="The Dime Logo" height="40" className="me-2" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link" href="/foundations">Foundations</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">About</a>
+              <a className="nav-link" href="/tokens">Tokens</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">Contact</a>
+              <a className="nav-link" href="/defi">DeFi</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/web3">Web3</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/safety">Safety</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/thedime">Using TheDime</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/community">Community</a>
             </li>
           </ul>
         </div>
